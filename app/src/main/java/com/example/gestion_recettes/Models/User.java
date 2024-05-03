27688846633538a317092f6675e68dba17e00a5c
OussamaZermouq <oussamazermouq2@gmail.com>
2024-05-04 00:45:09 +0100

    Working on the home page

--- app/src/main/java/com/example/gestion_recettes/Models/User.java
+++ app/src/main/java/com/example/gestion_recettes/Models/User.java
@@ -5,7 +5,8 @@ public class User {
     private int user_id;
     private String username;
     private String password;
-
+    public String user_image;
+    
     public int getUser_id() {
         return user_id;
     }
